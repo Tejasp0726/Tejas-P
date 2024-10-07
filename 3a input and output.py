ee3ccd9ba6ac1c35e3750c9460eb8349f31608f9
Tejas P <tejasteju0726@gmail.com>
2024-10-07 17:38:01 +0530

    Add files via upload

---
+++ 3a input and output.py
@@ -0,0 +1,9 @@
+print("Lifestyle")
+_Name = input("Name : ")
+_Age = int(input("Age: "))
+_Salary = int(input("Salary (in dollars): "))
+_Expenditure = int(input("Expenditure:  "))
+#single line comment
+_Savings = abs(_Salary - _Expenditure)
+print (_Name  +  " of age " +  str(_Age)  + " ,salary of " + str(_Salary) +  " and spends upto "  + str(_Expenditure) +  " finally saves " +str(_Savings) )
+ 
